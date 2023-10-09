7d8e413c50996540ff4bbce05e09a54f328e8cc1
Marzerp <araceliromerozerpa@gmail.com>
2023-10-09 12:57:45 -0600

    upgraded version

--- maximum.py
+++ maximum.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env pyhton3
-
 #Maximum algorythm
 #By Marzerp
 #LICENSE GNU/GPL
 
+import sys
+
 data= [1.0, 3.14, 6.2, 0.1, 5.3]
 
-maximum= -99999.999
+maximum= -sys.float_info.max
 
 for x in data:
     if x>maximum:
