d12b7aad58841b80b69ffc13ea55c4a2a21ff049
Marzerp <araceliromerozerpa@gmail.com>
2023-10-09 12:58:49 -0600

    upgraded version

--- minimum.py
+++ minimum.py
@@ -4,12 +4,14 @@
 #By: Marzerp
 #LICENSE GNU/GPL
 
-minimum= 9999999.99
+import sys
+
+minimum= sys.float_info.max
 
 data=[1.0, 3.14, 6.2, 0.1, 5.3]
 
 for x in data:
     if x<minimum:
-    minimum=x
+        minimum=x
 
 print(minimum)
