44d13573f01c6794a5277cd9adcfe7e61385e395
Marzerp <araceliromerozerpa@gmail.com>
2023-10-09 12:47:43 -0600

    mimimum algorythm

---
+++ minimum.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+#Maximum algorythm
+#By: Marzerp
+#LICENSE GNU/GPL
+
+minimum= 9999999.99
+
+data=[1.0, 3.14, 6.2, 0.1, 5.3]
+
+for x in data:
+    if x<minimum:
+    minimum=x
+
+print(minimum)
